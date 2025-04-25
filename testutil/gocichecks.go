c2ddb462d976830d613040c043c5126f185beba2
Coder Robot <ben+bot3@coder.com>
2025-04-25 20:55:15 +0000

    test: add another Go file to force CI checks

---
+++ testutil/gocichecks.go
@@ -0,0 +1,8 @@
+package testutil
+
+// This file is only here to test the CI workflow's go checks
+
+// GoTestFunction is specifically made to trigger the Go CI checks
+func GoTestFunction() string {
+	return "This function should trigger the Go CI checks"
+}
